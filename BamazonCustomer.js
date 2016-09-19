ac2777ddb876bfe20ee3beb9845945c87f5e5ab8
Antonio Montalvo <montalvocode@yahoo.com>
2016-09-19 14:34:47 -0700

    Challenge #1 completed

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,4 +1,8 @@
 //npm install mysql
+//npm install inquirer
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var productsID;
 
 var mysql = require('mysql') ;
 var connection = mysql.createConnection({
@@ -16,4 +20,35 @@ connection.connect(function(err) {
 connection.query('SELECT * FROM Products', function(err, res) {
     if (err) throw err;
     console.log(res);
-})
+
+    inquirer.prompt([
+		{
+			name: "ID",
+			message: "What's the ItemID of the product you would like to purchase?"
+		},
+		{
+			name: "Units",
+			message: "How many units of this product would you like to purchase?"
+		}
+	]).then(function(answer) {
+		var selected = answer.ID ;
+		connection.query('SELECT * FROM Products WHERE ItemID = ?', selected, function(err, res) {
+			if(res[0].StockQuantity >= answer.Units){
+				console.log('Congratulations on your purchase! The total cost is $' + res[0].Price * answer.Units);
+				var leftInStock = res[0].StockQuantity - answer.Units;
+				connection.query('UPDATE Products SET ? WHERE ?', [{
+					StockQuantity: leftInStock
+				},
+				{
+					ItemID: answer.ID	
+				}], function(err, res) {}
+					);
+			}else{
+				console.log("We apologize but don't have that many items in stock!");
+				return;
+			}
+		})
+	});
+});
+
+
